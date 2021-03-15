1d050408aa57a7340d3bc5786a7c9bfa8dc2126d
hiten <hiten@gmail.com>
2021-03-15 13:59:29 +0530

    first commit

---
+++ soft.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update -y
+#sudo apt-get upgrade -y
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://repo.skype.com/latest/skypeforlinux-64.deb
+sudo dpkg -i *.deb
+sudo apt-get install -f -y
+sudo apt-get install git maven openjdk-8-jdk -y
+sudo snap install zoom-client
+sudo snap install slack --classic
