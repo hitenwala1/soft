5eef3befd57558a44a53fcf22269be5944492880
hiten <hiten@gmail.com>
2021-03-15 16:41:41 +0530

    second

--- soft.sh
+++ soft.sh
@@ -4,8 +4,9 @@ sudo apt-get update -y
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
 sudo dpkg -i *.deb
 sudo apt-get install -f -y
 sudo apt-get install git maven openjdk-8-jdk -y
 sudo snap install zoom-client
-sudo snap install slack --classic
+#sudo snap install slack --classic
