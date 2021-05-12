54641a3b2d5969fdf02a0278ab6fa0b7bd2b3e25
hitenwala1 <79907281+hitenwala1@users.noreply.github.com>
2021-05-12 16:24:48 +0530

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install anydesk -y
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
