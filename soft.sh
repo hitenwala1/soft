0e6124727ff0a87b6f51360b1d941766adc67afc
hitenwala1 <79907281+hitenwala1@users.noreply.github.com>
2021-05-12 16:27:28 +0530

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install wget
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 
 sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo apt-get install anydesk -y
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -13,4 +15,6 @@ sudo dpkg -i *.deb
 sudo apt-get install -f -y
 sudo apt-get install git maven openjdk-8-jdk -y
 sudo snap install zoom-client
+sudo apt-get upgrade -y
+
 #sudo snap install slack --classic
