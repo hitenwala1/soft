b5a0a9976606600b3c8427b8a5d54bfd6970b051
hitenwala1 <79907281+hitenwala1@users.noreply.github.com>
2021-08-13 11:30:55 +0530

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -16,6 +16,7 @@ sudo dpkg -i *.deb
 sudo apt-get install -f -y
 sudo apt-get install git maven openjdk-8-jdk -y
 sudo snap install zoom-client
+sudo apt-get update -y
 sudo apt-get upgrade -y
 
 #sudo snap install slack --classic
