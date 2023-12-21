790b8f50e2fe05c450a3f51fbb2789f147de664b
hitenwala1 <79907281+hitenwala1@users.noreply.github.com>
2023-12-21 10:46:26 +0530

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -11,7 +11,6 @@ sudo apt-get install gnome-session-flashback -y
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
 sudo dpkg -i *.deb
 sudo apt-get install -f -y
 sudo apt-get install git maven openjdk-8-jdk -y
@@ -19,5 +18,4 @@ sudo snap install zoom-client
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install openssh-server openssh-client -y
-
-#sudo snap install slack --classic
+sudo snap install slack --classic
